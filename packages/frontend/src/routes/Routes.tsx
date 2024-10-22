4e69389a2d7eb82bccfb4ef5aa928e2312c68e57
tobySolutions <adedejitobiloba7@gmail.com>
2024-10-22 11:08:09 +0100

    feat: add auth pages

--- packages/frontend/src/routes/Routes.tsx
+++ packages/frontend/src/routes/Routes.tsx
@@ -3,12 +3,14 @@ import Home from "./home";
 import Login from "./login";
 import DashboardHome from "./dashboard/home";
 import Projects from "./dashboard/projects";
+import SignUp from "./signup";
 
 export default function Routes() {
   return (
     <BaseRoutes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/dashboard">
         <Route index element={<DashboardHome />} />
         <Route path="projects" element={<Projects />} />
