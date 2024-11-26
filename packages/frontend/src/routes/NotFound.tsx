2b5d3bfa9de48fc6e4c0e03216d4072d4fb99dc1
tobySolutions <adedejitobiloba7@gmail.com>
2024-11-27 00:22:12 +0100

    chore: checkout to add 404 pages

---
+++ packages/frontend/src/routes/NotFound.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div className="bg-black">
+      <div className="min-h-screen flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8 text-center">
+          <div className="space-y-4">
+            <h1 className="text-9xl font-extrabold text-primary-white">404</h1>
+            <h2 className="text-4xl font-bold text-yellow-dark-12">
+              Page Not Found
+            </h2>
+            <p className="text-xl text-yellow-dark-12">
+              Oops! The page you are looking for doesn't exist or has been
+              moved.
+            </p>
+          </div>
+          <div className="mt-8">
+            <Link
+              to="https://stream2peer.on-fleek.app/"
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+            >
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
