e350fe58ad5b26181040b5f73df9b74327220d4b
excellencyjumo <excellencejumo@gmail.com>
2024-11-27 19:37:02 +0100

    feat: user-profile

--- packages/backend/src/Api/Modules/Client/Authentication/Routes/index.ts
+++ packages/backend/src/Api/Modules/Client/Authentication/Routes/index.ts
@@ -3,11 +3,13 @@ import GoogleAuthRoutes from './GoogleAuth';
 import GithubAuthRoutes from './GithubAuth';
 import MetaMaskAuthRoutes from './MetaMaskAuth';
 import EmailAuthRoutes from './EmailAuth';
+import ProfileRoutes from './ProfileAuth';
 const routes = Router();
 
 routes.use('', GoogleAuthRoutes);
 routes.use('', GithubAuthRoutes);
 routes.use('', MetaMaskAuthRoutes);
 routes.use('', EmailAuthRoutes);
+routes.use('', ProfileRoutes);
 
 export default routes;
